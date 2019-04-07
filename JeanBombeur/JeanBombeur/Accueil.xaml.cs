efd3bc2bbcd5785309637814fcda393835e144ae
AdrienBlay <AdrienBlay@346c1dad-1360-44a5-8053-76309fac6d35>
2019-04-07 17:52:39 +0000

    Cas d'utilisation WORD, balsamiq et debut de code sur Visual Studio

---
+++ JeanBombeur/JeanBombeur/Accueil.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JeanBombeur
+{
+    
+    public partial class Accueil : Page
+    {
+        MainWindow window;
+        public Accueil()
+        {
+            InitializeComponent();
+        }
+
+        public Accueil(MainWindow w) : this()
+        {
+            window = w;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            window.PageFrame.Navigate(new Categorie());
+        }
+    }
+}
---
+++ JeanBombeur/JeanBombeur/Categorie.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JeanBombeur
+{
+    /// <summary>
+    /// Logique d'interaction pour Categorie.xaml
+    /// </summary>
+    public partial class Categorie : Page
+    {
+        public Categorie()
+        {
+            InitializeComponent();
+        }
+
+        private void Sandwich(object sender, RoutedEventArgs e)
+        {
+            Sandwich s = new Sandwich();
+            this.NavigationService.Navigate(s);
+        }
+
+        private void Pizza(object sender, RoutedEventArgs e)
+        {
+            Pizza p = new Pizza();
+            this.NavigationService.Navigate(p);
+        }
+
+        private void Tacos(object sender, RoutedEventArgs e)
+        {
+            Tacos t = new Tacos();
+            this.NavigationService.Navigate(t);
+        }
+    }
+}
