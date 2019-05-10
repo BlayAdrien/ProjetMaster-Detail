740e93264a5dc88f967208bf05b2f89e1ff8fe7a
AdrienBlay <AdrienBlay@346c1dad-1360-44a5-8053-76309fac6d35>
2019-05-10 15:26:50 +0000

    Le stub marche mais pas le binding faudra revoir je vois pas trop ou est l'erreur

--- JeanBombeur/Donnee/FabriqueCategorieStub.cs
+++ JeanBombeur/Donnee/FabriqueCategorieStub.cs
@@ -13,15 +13,27 @@ namespace Donnee
         public override Restaurant CreateCategorie()
         {
             Restaurant r = new Restaurant();
+            List<Ingredient> listReine = new List<Ingredient>
+            {
+                new Ingredient("Jambon", new Quantite(10, Unite.cl)),
+                new Ingredient("Oeuf", new Quantite(10, Unite.cl))
+            };
+
+            List<Ingredient> listHawaienne = new List<Ingredient>
+            {
+                new Ingredient("Jambon", new Quantite(10, Unite.cl)),
+                new Ingredient("Oeuf", new Quantite(10, Unite.cl)),
+                new Ingredient("Ananas", new Quantite(10, Unite.cl)),
+
+            };
 
 
 
             r.AjouterPlat(
-                new Plat("Pizza", "Hawaienne", 10, new List<Ingredient>() ));
+                new Plat("Pizza", "Reine", 10, listReine ));
+                new Plat("Pizza", "Hawaienne", 10, listHawaienne);
             return r;
 
         }
     }
 }
-
-//, Jambon, Fromage, Sauce Tomate 
